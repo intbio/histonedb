a4d0369536ac71bd21c6c212edd590028037fa7a
l-singh-biomsu <52165734+l-singh-biomsu@users.noreply.github.com>
2024-08-29 17:09:36 +0300

    corrected setup intructions for py37

--- system_setup/full_setup.sh
+++ system_setup/full_setup.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # System Setup
-yes Y | sh system_setup/sys_requirements_setup.txt
+yes Y | sh system_setup/sys_requirements.txt
 sudo sed -i '$a wait_timeout = 31536000' /etc/mysql/mysql.conf.d/mysqld.cnf
 sudo sed -i '$a interactive_timeout = 31536000' /etc/mysql/mysql.conf.d/mysqld.cnf
 echo 'System setup complete.'
 
 # Python Requirements Setup
-virtualenv histdb_py27
-source histdb_py27/bin/activate
-echo 'Virtualenv created and set to histdb_py27.'
+virtualenv -p python3.7 venv_histdb_py37
+source venv_histdb_py37/bin/activate
+echo 'Virtualenv created and set to venv_histdb_py37.'
 #yes Y | sh system_setup/py_requirements_setup.txt
 pip install --yes --no-cache-dir -r system_setup/py_requirements.txt
 echo 'Python setup complete.'
